097bb38cd8a231edc447b9c67522d4746feb571e
Frode Magnussen <frode.magnussen@capgemini.com>
2022-12-26 20:57:14 +0100

    Add project files.

---
+++ WiredBrainCoffee.DataProsessorTests/Parsing/CsvLineParserTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace WiredBrainCoffee.DataProcessor.Parsing;
+
+public class CsvLineParserTests
+{
+    [Fact]
+    public void ShouldParseValidLine()
+    {
+        // Arrange
+        string[] csvLines = new[] { "Cappuccino;27/10/2022 8:06:04 AM" };
+
+        // Act
+        var machineDataItems = CsvLineParser.Parse(csvLines);
+
+        // Assert
+        Assert.NotNull(machineDataItems);
+        Assert.Single(machineDataItems);
+        Assert.Equal("Cappuccino", machineDataItems[0].CoffeeType);
+        Assert.Equal(new System.DateTime(2022, 10, 27, 8, 6, 4), machineDataItems[0].CreatedAt);
+    }
+
+    [Fact]
+    public void ShouldSkipEmptyLines()
+    {
+        // Arrange
+        string[] csvLines = new[] { "", " " };
+
+        // Act
+        var machineDataItems = CsvLineParser.Parse(csvLines);
+
+        // Assert
+        Assert.NotNull(machineDataItems);
+        Assert.Empty(machineDataItems);
+    }
+
+    [InlineData("Cappuccino", "Invalid csv line")]
+    [InlineData("Cappuccino;InvalidDateTime", "Invalid datetime in csv line")]
+    [Theory]
+    public void ShouldThrowExceptopmForInvalidLine(string csvLine, string expectedMessagePrefix)
+    {
+        // Arrange
+        var csvLines = new[] { csvLine };
+
+        // Act
+        var exception = Assert.Throws<Exception>(() => CsvLineParser.Parse(csvLines));
+
+        Assert.Equal($"{expectedMessagePrefix}: {csvLine}", exception.Message);
+    }
+
+    [Fact]
+    public void ShouldThrowExceptopmForInvalidLine2()
+    {
+        // Arrange
+        var csvLine = "Cappuccino;InvalidDateTime";
+        var csvLines = new[] { csvLine };
+
+        // Act
+        var exception = Assert.Throws<Exception>(() => CsvLineParser.Parse(csvLines));
+
+        Assert.Equal($"Invalid datetime in csv line: {csvLine}", exception.Message);
+    }
+}
+
